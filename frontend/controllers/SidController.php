5edf4026b5d5f021b1d4478c4db3b91089362805
DimachDim <dakuznecov45@gmail.com>
2024-03-21 16:51:36 +0500

    Создание логики авторизации

--- frontend/controllers/SidController.php
+++ frontend/controllers/SidController.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace frontend\controllers;
+use Yii;
 use yii\rest\ActiveController;
 use yii\helpers\ArrayHelper;
+// models
+use common\models\Sid;
+use common\models\Users;
+
+include '../functions/generateRandomString.php';
 
 class SidController extends ActiveController
 {
@@ -38,7 +44,40 @@ class SidController extends ActiveController
     public function actionCreate()
     {
         try{
-            return ['ok'=>'ok'];        
+            
+            // Вытаскиваем данные запроса
+            $userName = Yii::$app->request->post('userName');
+            $password = Yii::$app->request->post('password');
+
+            // Запросы к БД
+            $user = Users::findOne(['username' => $userName]);
+
+            // Если есть такой пользователь
+            if($user != null)
+            {
+                // Если пароль подходит
+                if($user->password == $password)
+                {
+                    // Создаем сессию
+                    $sid = new Sid();
+                    $randomString = generateRandomString(rand(9,20));
+                    $sid->sid = $randomString;
+                    $sid->id_user = $user->id;
+                    $sid->save();
+
+                    // Отправляем сессию пользователю
+                    return ['sid' => $sid->sid];
+                
+                // Если пароль не подходит
+                }else{
+                    throw new \Exception('Неверный пароль.');
+                }
+            
+            // Нет такого пользователя
+            }else{
+                throw new \Exception('Такой пользователь не зарегистрирован.');
+            }
+
         }catch(\Exception $e){
             return ['errorText' => $e->getMessage()];
         }
