b37a7c76ff08cd81736705003c8550236a1142f9
DimachDim <dakuznecov45@gmail.com>
2024-03-25 22:30:58 +0500

    Обработка для обращения domain/invite-tokens post

--- frontend/controllers/InviteTokensController.php
+++ frontend/controllers/InviteTokensController.php
@@ -5,8 +5,10 @@ namespace frontend\controllers;
 use Yii;
 use yii\rest\ActiveController;
 use yii\helpers\ArrayHelper;
-
+include '../functions/generateRandomString.php';
+//models
 use common\models\InvitationTokens;
+use common\models\Users;
 
 class InviteTokensController extends ActiveController
 {
@@ -43,9 +45,42 @@ class InviteTokensController extends ActiveController
     {
         try{
             
-            
+            // id пользователя
+            $userId = Yii::$app->request->post('userId');
 
-            return 'ok';
+            // Создаем токен
+            $tokenRandom = generateRandomString(10, '#');
+            // Создаем запись в бд с таким токеном
+            $inviteToken = new InvitationTokens();
+            $inviteToken->id_inviting_user = $userId;
+            $inviteToken->id_invited_user = null;
+            $inviteToken->token = $tokenRandom;
+            $inviteToken->save();
+            
+            // Получаем все созданные пользователем токены
+            $userTokens = InvitationTokens::findAll(['id_inviting_user'=>$userId]);
+            $newUserTokens = [];    // Для копирования массива
+
+            // Перебераем полученный массив
+            foreach($userTokens as $token)
+            {   
+                // Конвертируем объект в массив
+                $newToken = $token->attributes;
+                // Если есть приглашенный пользователь
+                if($token->id_invited_user != null)
+                {
+                    // Добавляем новое свойство и запрашиваем в базе имя пользователя
+                    $newToken['name_invited_user'] = Users::findOne(['id' => $token->id_invited_user])->username;
+                }
+
+                // Добавляем измененый объект в массив
+                $newUserTokens[] = $newToken;
+            }
+
+            // Возвращаем новый токен и все токены пользователя
+            return ['token' => $tokenRandom, 'tokens' => $newUserTokens];
+
+           
 
         }catch(\Exception $e){
             return ['errorText' => $e->getMessage()];
