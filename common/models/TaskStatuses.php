0d2ca8560138ccd48478b419bd188f638d7993aa
DimachDim <dakuznecov45@gmail.com>
2024-03-27 18:12:21 +0500

    Обновление модели tasks и зоздание task_statuses

---
+++ common/models/TaskStatuses.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "task_statuses".
+ *
+ * @property int $id
+ * @property string|null $statusName
+ *
+ * @property Tasks[] $tasks
+ */
+class TaskStatuses extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'task_statuses';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['statusName'], 'string', 'max' => 250],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'statusName' => 'Status Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[Tasks]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTasks()
+    {
+        return $this->hasMany(Tasks::class, ['id_status' => 'id']);
+    }
+}
--- common/models/Tasks.php
+++ common/models/Tasks.php
@@ -12,8 +12,14 @@ use Yii;
  * @property string $title
  * @property string $text
  * @property int|null $id_user_creator
+ * @property int|null $id_user_executor
+ * @property string|null $date_start
+ * @property string|null $date_end
+ * @property int|null $id_status
  *
+ * @property TaskStatuses $status
  * @property Users $userCreator
+ * @property Users $userExecutor
  */
 class Tasks extends \yii\db\ActiveRecord
 {
@@ -33,9 +39,12 @@ class Tasks extends \yii\db\ActiveRecord
         return [
             [['token', 'title', 'text'], 'required'],
             [['text'], 'string'],
-            [['id_user_creator'], 'integer'],
+            [['id_user_creator', 'id_user_executor', 'id_status'], 'integer'],
+            [['date_start', 'date_end'], 'safe'],
             [['token', 'title'], 'string', 'max' => 250],
             [['id_user_creator'], 'exist', 'skipOnError' => true, 'targetClass' => Users::class, 'targetAttribute' => ['id_user_creator' => 'id']],
+            [['id_user_executor'], 'exist', 'skipOnError' => true, 'targetClass' => Users::class, 'targetAttribute' => ['id_user_executor' => 'id']],
+            [['id_status'], 'exist', 'skipOnError' => true, 'targetClass' => TaskStatuses::class, 'targetAttribute' => ['id_status' => 'id']],
         ];
     }
 
@@ -50,7 +59,21 @@ class Tasks extends \yii\db\ActiveRecord
             'title' => 'Title',
             'text' => 'Text',
             'id_user_creator' => 'Id User Creator',
+            'id_user_executor' => 'Id User Executor',
+            'date_start' => 'Date Start',
+            'date_end' => 'Date End',
+            'id_status' => 'Id Status',
         ];
+    }
+
+    /**
+     * Gets query for [[Status]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStatus()
+    {
+        return $this->hasOne(TaskStatuses::class, ['id' => 'id_status']);
     }
 
     /**
@@ -61,5 +84,15 @@ class Tasks extends \yii\db\ActiveRecord
     public function getUserCreator()
     {
         return $this->hasOne(Users::class, ['id' => 'id_user_creator']);
+    }
+
+    /**
+     * Gets query for [[UserExecutor]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserExecutor()
+    {
+        return $this->hasOne(Users::class, ['id' => 'id_user_executor']);
     }
 }
