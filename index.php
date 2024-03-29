e7aa48aa5cc82269ea19e35fce634a32d719ec58
DimachDim <dakuznecov45@gmail.com>
2024-03-30 01:06:55 +0500

    Добавление места для проекта react

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Проверяем URI, если он не соответствует нашему пути frontend/web
-if ($_SERVER['REQUEST_URI'] != '/frontend/api') {
-    // Перенаправляем запрос на путь frontend/web
-    header('Location: /frontend/api' . $_SERVER['REQUEST_URI']);
-    exit;
-}
